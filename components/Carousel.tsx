8a4f049e684e1d53d783bd54f6721ee48092c90d
arifd <ademirkan@ucsd.edu>
2025-01-20 14:33:06 -0800

    ui: added gradient to carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Carousel.module.css";
 
 interface CarouselProps {
     imgSrcs: string[];
@@ -8,26 +9,34 @@ export default function Carousel({ imgSrcs }: CarouselProps) {
     if (!imgSrcs || imgSrcs.length === 0) return null;
 
     return (
-        <div className="relative flex left-[calc(var(--padding)*-1)] px-[var(--padding)] gap-6  no-scrollbar p-5 w-[calc(100vw-16px)] overflow-x-auto no-scrollbar overscroll-auto">
-            {imgSrcs.map((src, index) => (
-                <div
-                    key={index}
-                    className="flex-shrink-0 w-80 aspect-[1.78] hover:cursor-pointer"
-                >
-                    <img
-                        src={src}
-                        alt={`Carousel image ${index + 1}`}
-                        className="relative w-full h-full object-cover shadow-lg shadow-black rounded-[4px] hover:scale-105 duration-300 ease-out outline outline-[3px] outline-offset-4 outline-transparent hover:outline-white hover:z-10 transition-all"
-                        onError={(e) => {
-                            const container = e.currentTarget.parentElement;
-                            if (container) {
-                                container.style.display = "none";
-                                container.style.position = "absolute";
-                            }
-                        }}
-                    />
-                </div>
-            ))}
+        <div className="relative w-[100vw] -ml-[calc((100vw-100%)/2)]">
+            <div
+                className={`
+                    relative flex gap-6 no-scrollbar p-5 w-[calc(100vw)]
+                    overflow-x-auto overscroll-auto px-12
+                    ${styles.maskGradient}
+                `}
+            >
+                {imgSrcs.map((src, index) => (
+                    <div
+                        key={index}
+                        className="flex-shrink-0 w-80 aspect-[1.78] hover:cursor-pointer"
+                    >
+                        <img
+                            src={src}
+                            alt={`Carousel image ${index + 1}`}
+                            className="relative w-full h-full object-cover shadow-lg shadow-black rounded-[4px] hover:scale-105 duration-300 ease-out outline outline-[3px] outline-offset-4 outline-transparent hover:outline-white hover:z-10 transition-all"
+                            onError={(e) => {
+                                const container = e.currentTarget.parentElement;
+                                if (container) {
+                                    container.style.display = "none";
+                                    container.style.position = "absolute";
+                                }
+                            }}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
