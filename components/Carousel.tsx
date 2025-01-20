d72da9364ed1bcc9eae1b3001b205c1375d838f6
arifd <ademirkan@ucsd.edu>
2025-01-20 11:53:57 -0800

    fix: carousel remove unfetched images

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -4,34 +4,30 @@ interface CarouselProps {
     imgSrcs: string[];
 }
 
-const Carousel = ({ imgSrcs }: CarouselProps) => {
+export default function Carousel({ imgSrcs }: CarouselProps) {
     if (!imgSrcs || imgSrcs.length === 0) return null;
 
     return (
-        <div className="overflow-x-visible">
-            <div className="flex flex-row gap-4 overflow-x-scroll no-scrollbar p-4">
-                {imgSrcs.map((src, index) => (
-                    <div
-                        key={index}
-                        className="transition-transform duration-200 hover:scale-110"
-                    >
-                        <img
-                            className="aspect-[1.78] drop-shadow-lg shadow-pink-500"
-                            src={src}
-                            alt={`Carousel image ${index + 1}`}
-                            onError={(e) => {
-                                const container = e.currentTarget.parentElement;
-                                if (container) {
-                                    container.style.display = "none";
-                                    container.style.position = "absolute";
-                                }
-                            }}
-                        />
-                    </div>
-                ))}
-            </div>
+        <div className="relative flex left-[calc(var(--padding)*-1)] pl-[var(--padding)] gap-6 overflow-x-auto no-scrollbar p-3 w-[100vw]">
+            {imgSrcs.map((src, index) => (
+                <div
+                    key={index}
+                    className="flex-shrink-0 w-60 aspect-[1.78] snap-center hover:cursor-pointer hover:scale-110"
+                >
+                    <img
+                        src={src}
+                        alt={`Carousel image ${index + 1}`}
+                        className="relative w-full h-full object-cover shadow-lg shadow-black/50 rounded-[4px]"
+                        onError={(e) => {
+                            const container = e.currentTarget.parentElement;
+                            if (container) {
+                                container.style.display = "none";
+                                container.style.position = "absolute";
+                            }
+                        }}
+                    />
+                </div>
+            ))}
         </div>
     );
-};
-
-export default Carousel;
+}
