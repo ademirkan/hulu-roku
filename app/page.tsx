af70b8261158fc87443a9ee1f9e10579c339490f
arifd <ademirkan@ucsd.edu>
2025-01-20 23:27:11 -0800

    ui: navbar and avatar added to header

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,33 @@ export default async function Home() {
     const collections = await fetchCollections();
     return (
         <>
-            <header className="fixed top-0 left-0 w-full bg-nav z-20 px-[var(--padding)] py-4">
-                <Image src="/disney.png" alt="logo" width={100} height={100} />
+            <header className="fixed flex flex-row items-center justify-between top-0 left-0 w-full bg-nav z-20 px-[var(--padding)] py-4">
+                <div className="flex flex-row items-center gap-12">
+                    <Image
+                        src="/disney.png"
+                        alt="logo"
+                        width={75}
+                        height={100}
+                        draggable={false}
+                    />
+                    <nav className="relative top-1 hover:cursor-pointer">
+                        <Image
+                            draggable={false}
+                            src="/navbar.png"
+                            alt="search"
+                            width={750}
+                            height={100}
+                        />
+                    </nav>
+                </div>
+                <Image
+                    draggable={false}
+                    src="/avatar.png"
+                    alt="avatar"
+                    width={50}
+                    height={50}
+                    className="hover:cursor-pointer rounded-full"
+                />
             </header>
             <div className="flex flex-col gap-8 pt-32">
                 {collections.length > 0 ? (
